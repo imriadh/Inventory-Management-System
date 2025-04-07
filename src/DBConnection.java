612bcaf7e467416babe075b656ac93ea422edf27
Riad Hossain <160244849+imriadh@users.noreply.github.com>
2025-04-07 19:54:10 +0600

    Update DBConnection.java

--- src/DBConnection.java
+++ src/DBConnection.java
@@ -4,7 +4,7 @@ import java.sql.SQLException;
 
 public class DBConnection {
     private static final String JDBC_URL = "jdbc:oracle:thin:@localhost:1521:xe"; // Replace with your Oracle details
-    private static final String USERNAME = "alex";
+    private static final String USERNAME = "Riad";
     private static final String PASSWORD = "1234";
 
     public static Connection getConnection() throws SQLException {
